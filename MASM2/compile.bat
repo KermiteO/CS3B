f7037f436bed7534a9d0fcc92127efd3e24d840f
KermiteO <kermicheo@gmail.com>
2018-03-27 15:42:10 -0700

    Delete compile.bat

--- MASM2/compile.bat
+++
@@ -1,5 +0,0 @@
-set projectName=Masm2
-ml /c /Zd /coff /Fl %projectName%.asm
-Link /SUBSYSTEM:CONSOLE -entry:MAIN %projectName%.obj ../macros/convutil201604.obj ../macros/utility201609.obj
-del %projectName%.obj
-%projectName%.exe
